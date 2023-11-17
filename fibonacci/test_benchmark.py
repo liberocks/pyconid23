ea5c822fe4f2254e716fc1eace68f36ec06c0b12
Tirtadwipa Manunggal <tirtadwipa.manunggal@gmail.com>
2023-11-17 14:45:14 +0000

    fix typo

--- fibonacci/test_benchmark.py
+++ fibonacci/test_benchmark.py
@@ -2,13 +2,13 @@ import fibonacci
 import rust_fibonacci
 
 
-def test_python_prime_factorization(benchmark):
+def test_python_fibonacci(benchmark):
     result = benchmark(fibonacci.fibonacci, 32)
 
     assert result == 2178309
 
 
-def test_rust_prime_factorization(benchmark):
+def test_rust_fibonacci(benchmark):
     result = benchmark(rust_fibonacci.fibonacci, 32)
 
     assert result == 2178309
